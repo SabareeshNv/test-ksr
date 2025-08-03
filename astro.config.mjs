cd28773959302cb69f57041a2140d6c392103d81
Sabareesh N V <sabareeshnv@gmail.com>
2025-08-03 19:17:20 +0530

    added sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,22 +2,16 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import pagefind from "astro-pagefind";
-
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
     site: "https://test-ksr.netlify.app",
-    // markdown: {
-    //     // Enable raw HTML in markdown files
-    //     rehypePlugins: [],
-    //     remarkPlugins: [],
-    //     syntaxHighlight: false, // optional
-    // },
     build: {
         assets: "_assets",
     },
     vite: {
         plugins: [tailwindcss()],
     },
-    integrations: [pagefind()],
+    integrations: [sitemap(), pagefind()],
 });
